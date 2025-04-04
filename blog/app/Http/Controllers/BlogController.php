b58656272ed677f3a8e0428e439e1236056f4e88
Marcos <markitosanches@gmail.com>
2025-04-03 21:22:01 -0400

    Class 1

---
+++ blog/app/Http/Controllers/BlogController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class BlogController extends Controller
+{
+    public function index(){
+        return view('home');
+    }
+
+    public function about(){
+        return view('about');
+    }
+}
